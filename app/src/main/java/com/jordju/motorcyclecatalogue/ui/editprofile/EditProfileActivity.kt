03768aea899fb1e55d64522fb19c675cacddff79
JordJ19 <jordie.juwono96@gmail.com>
2023-05-17 01:52:44 +0700

    add edit profile activity

---
+++ app/src/main/java/com/jordju/motorcyclecatalogue/ui/editprofile/EditProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.jordju.motorcyclecatalogue.ui.editprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
